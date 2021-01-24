39d2c19aab6e2d5654b6ebfe9273c4ed9b68bbe9
mrizkiprmn <rizkiypermana@gmail.com>
2021-01-25 01:24:24 +0700

    change

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -62,7 +62,7 @@ export default {
    
     methods: {
       login() {
-            Axios.post("http://54.160.107.173/api/auth")
+            Axios.post("http://54.160.107.173:8283/api/auth")
             .then((res) => {
         if(res.data.result[0].msg === 'Email belum terdaftar') {
           return alert('Email belum terdaftar');
